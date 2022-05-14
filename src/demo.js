d0992628bd3f6db19ce8bedead5f62b254d6dd1b
Aleksandr Gontcharov <agontcharov@microsoft.com>
2022-05-14 14:55:39 -0700

    Create basic pixiJS game

--- src/demo.js
+++
@@ -1,8 +0,0 @@
-const text = 'it work\'s!';
-
-const itWorks = () => {
-    console.log(text);
-    return text;
-};
-
-export default itWorks;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-import itWorks from './demo';
-import './demo.css';
+var PIXI = require('pixi.js');
 
-document.getElementById('root').textContent = itWorks();
+const Application = PIXI.Application;
+const app = new Application({ width: 500, 
+                              height: 500, 
+                              transparent: false,
+                              antialias: true});
+
+app.renderer.backgroundColor = 0x000000;
+
+app.renderer.resize(window.innerWidth, window.innerHeight);
+
+app.renderer.view.style.position = "absolute";
+
+document.body.appendChild(app.view);
+
+const Graphics = PIXI.Graphics;
