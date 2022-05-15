0cfac9499158892fa84088839dca20c227588aed
Aleksandr Gontcharov <agontcharov@microsoft.com>
2022-05-14 19:07:59 -0700

    refactor code

--- src/Game/Slider.js
+++ src/Game/Slider.js
@@ -20,7 +20,7 @@ export class Slider {
       // If currPosition and direction are not provided then generate them
       if (currPosition == undefined)
       {
-          currPosition = Math.floor(Math.random() * (5))  + 1;
+          currPosition = Math.floor(Math.random() * (height))  + 1;
       }
       if (direction == undefined)
       {
@@ -32,33 +32,16 @@ export class Slider {
       this.direction = direction;
   }
 
-
   Next()
     {
         // edge cases
-        if (this.currPosition == this.height)
-        {
+        if (this.currPosition == this.height || !this.direction) {
             this.currPosition = this.currPosition - 1;
             this.constructorHelper(this.height, this.currPosition, false);
 
-        }
-        else if (this.currPosition == 1)
-        {
+        } else if (this.currPosition == 1 || this.direction) {
             this.currPosition = this.currPosition + 1;
             this.constructorHelper(this.height, this.currPosition, true);
-        }
-        else
-        { // in the middle of the slider
-            if (this.direction)
-            {
-                this.currPosition = this.currPosition + 1;
-                this.constructorHelper(this.height, this.currPosition, true);
-            }
-            else
-            {
-                this.currPosition = this.currPosition - 1;
-                this.constructorHelper(this.height, this.currPosition, false);
-            }
         }
     }
 }
