7b057f21567c681bbac72d6230622946c151e7ac
Aleksandr Gontcharov <alexgoncharov@gmail.com>
2022-08-10 19:45:04 -0700

    trying

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ var PIXI = require('pixi.js');
 const { Slider } = require('./Game/classes/Slider');
 const { Stage } = require('./Game/classes/Stage');
 const express = require('express');
-const os = require('os');
 import { drawBackground } from './Game/graphics/background';
 import { windowHeight, windowWidth } from './Game/graphics/windowLayout';
 import { levelIntro } from './Game/graphics/stage';
