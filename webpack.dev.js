edffe0c7e2c25f556c8a2f003eef58c8c84c34b6
Aleksandr Gontcharov <alexgoncharov@gmail.com>
2022-08-10 20:17:13 -0700

    trying

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,6 +6,7 @@ module.exports = merge(common, {
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
-        historyApiFallback: true
+        historyApiFallback: true,
+        allowedHosts: 'all'
     },
 });
