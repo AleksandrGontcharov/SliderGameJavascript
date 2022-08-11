b19760a1ea5de0d3081ea2ec62a3a254ddf1f3c6
Aleksandr Gontcharov <alexgoncharov@gmail.com>
2022-08-10 20:21:45 -0700

    change width

--- src/Game/graphics/windowLayout.js
+++ src/Game/graphics/windowLayout.js
@@ -1,5 +1,5 @@
 export let windowHeight = window.innerHeight;
-export let windowWidth = window.innerHeight * 0.8;
+export let windowWidth = Math.min(window.innerHeight * 0.8, window.innerWidth);
 export let margin_x = 0.1;
 export let margin_y = 0.07;
 export let slider_padding = 0.025;
